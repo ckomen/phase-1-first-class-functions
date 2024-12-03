0cef6abc11f3045496e0e24b8010cb641557fbb5
caren komen <cjepkorir09@gmail.com>
2024-12-03 18:53:55 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+    function returnsANamedFunction() {
+      return function namedFunction() {
+        console.log("named function");
+      };
+    }
+    
+    function returnsAnAnonymousFunction() {
+      return function() {
+        console.log("anonymous function");
+      };
+  }
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction,
+  };
+  
